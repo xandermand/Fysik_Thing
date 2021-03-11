742a11aa8567b2ac41fb06fd4a2021d6b8b481a2
xandermand <54107781+xandermand@users.noreply.github.com>
2021-03-11 13:34:08 +0100

    Update hub.py

--- hub.py
+++ hub.py
@@ -1,2 +1,14 @@
-import tkinter
-import matplotlib
+from tkinter import *
+
+#laver vinduet
+mainHub = Tk()
+
+hub = Label(mainHub, text="hello world")
+
+hub.grid(row=0, column=0)
+
+densButton = Button(mainHub, text="rør ved mig")
+
+densButton.grid(row=1, column=0)
+
+mainHub.mainloop()
