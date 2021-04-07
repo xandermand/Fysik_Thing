e1ea33ce29e3df2ed27a2bca3e038dca00051560
xandermand <54107781+xandermand@users.noreply.github.com>
2021-04-07 13:26:10 +0200

    Update tyngde.py

--- tyngde.py
+++ tyngde.py
@@ -1,6 +1,6 @@
 from tkinter import *
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
-While True:
-    a = input("skriv noget lort her")
-    print(a)
+
+a = input("skriv noget lort her")
+print(a)
