0f6c6db51c3bd5d02e54e591dd8e1a07d67595bd
PreussenBlue <45234020+PreussenBlue@users.noreply.github.com>
2021-04-08 13:07:07 +0200

    Update Densitet.py
    Lavede "templates" til de forskellige udregninger.

---
+++ Densitet.py
@@ -0,0 +1,30 @@
+import tkinkter as tk
+
+window = tk.Tk()
+
+
+
+#Finde densitet med masse og volumen
+
+def findDensitet():
+
+    masse = int()
+    volumen = int()
+
+    densitet = masse/volumen
+
+#Finde massen med densiteten og volumen
+
+def findMasse():
+    volumen = int()
+    densitet = int()
+
+    masse = densitet*volumen
+
+#find volumen
+
+def findVolumen():
+    masse = int()
+    densitet = int()
+
+    volumen = masse/densitet
