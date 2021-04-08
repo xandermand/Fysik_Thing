46397619cc071df017461f763e3a9829b74f8229
xandermand <54107781+xandermand@users.noreply.github.com>
2021-04-08 13:30:02 +0200

    Lave rbare videre på tyngde acceleration
    har lavet på mit eget, og en smule på hubben

--- hub.py
+++ hub.py
@@ -3,12 +3,16 @@ from subprocess import Popen
 #åbner et andet program
 def tungde():
     Popen('python tyngde.py')
+def dens():
+    Popen('python Densitet.py')
 
 #laver vinduet
 mainHub = Tk()
 
-space = Label(mainHub, text="           ")
-space2 = Label(mainHub, text="           ")
+mainHub.geometry('600x400')
+
+space = Label(mainHub, text="                     ")
+space2 = Label(mainHub, text="                     ")
 
 mennavn = Label(mainHub, text="Hoved Menu")
 
@@ -16,10 +20,17 @@ space.grid(row=0, column=0)
 mennavn.grid(row=0, column=1)
 space2.grid(row=0, column=2)
 
-densButton = Button(mainHub, text="Tyngdekraft", command=tungde)
+tungButton = Button(mainHub, text="Tyngdekraft", command=tungde)
 
 space.grid(row=1, column=0)
-densButton.grid(row=1, column=1)
+tungButton.grid(row=1, column=1)
 space2.grid(row=1, column=2)
+
+densButton = Button(mainHub, text="Densitet", command=dens)
+
+space.grid(row=2, column=0)
+densButton.grid(row=2, column=1)
+space2.grid(row=2, column=2)
+
 
 mainHub.mainloop()
--- tyngde.py
+++ tyngde.py
@@ -1,6 +1,19 @@
 from tkinter import *
+from tkinter.ttk import *
 #import matplotlib.pyplot as plt
+from subprocess import Popen
+
+#laver vores vindue
+tungHub = Tk()
+#sætter vores valgbox op
+comb = Combobox(tungHub)
+comb['values']=("Vælg en værdi", "Jorden", "Månen", "Jupiter", "Mekur", "Venus", "Angiv selv en tyngdeacceleration")
+comb.current(0)
+
+space = Label(tungHub, text="                     ")
+space2 = Label(tungHub, text="                     ")
 
 
-a = input("skriv noget lort her")
-print(a)
+comb.grid(column=0, row=0)
+
+tungHub.mainloop()
